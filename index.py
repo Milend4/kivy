1d2c4728fa7a599272f19b565af1202b8bd1d730
Milend4 <milenaleticia.15sa@gmail.com>
2024-02-23 23:20:02 -0300

    update

--- index.py
+++ index.py
@@ -85,7 +85,7 @@ class Cadastro_coletor(Screen):
     def cadastrar_coletor(self):
         nome = self.ids.nome.text
         genero = self.ids.genero.text
-        cpf = self.ids.cpf.text
+        usuario = self.ids.usuario.text
         tel = self.ids.tel.text
         empresa = self.ids.nome_empresa.text
         cnpj = self.ids.cnpj.text
@@ -95,8 +95,8 @@ class Cadastro_coletor(Screen):
         bairro = self.ids.bairro.text
         email = self.ids.email_cadastro.text
         senha = self.ids.senha_cadastro.text
-        if nome and email and senha and cpf and cnpj and empresa and genero and logadouro and bairro and estado and numero and tel:
-            if self.app.cadastrar_coletor(nome, genero, cpf, cnpj, empresa, tel, logadouro, numero, estado, bairro, email, senha):
+        if nome and email and senha and usuario and cnpj and empresa and genero and logadouro and bairro and estado and numero and tel:
+            if self.app.cadastrar_coletor(nome, genero, usuario, cnpj, empresa, tel, logadouro, numero, estado, bairro, email, senha):
                 self.ids.nome.text = ''
                 self.ids.genero.text = ''
                 self.manager.current = 'resistrar_coletor'
@@ -121,9 +121,10 @@ class Cadastro_usuario(Screen):
         bairro = self.ids.bairro.text
         email = self.ids.email_cadastro.text
         senha = self.ids.senha_cadastro.text
+        usuario = self.ids.usuario.text
         
-        if nome and email and senha and cpf_cnpj and genero and logadouro and bairro and estado and numero and tel :
-            if self.app.cadastrar_usuario(nome, genero, cpf_cnpj, tel, logadouro, numero, estado, bairro, email, senha):
+        if nome and email and senha and cpf_cnpj and genero and logadouro and bairro and estado and numero and tel and usuario :
+            if self.app.cadastrar_usuario(nome, genero, cpf_cnpj, tel, logadouro, numero, estado, bairro, email, senha, usuario):
                 self.ids.nome.text = ''
                 self.ids.genero.text = ''
                 self.manager.current = 'resistrar_usuario'
@@ -157,7 +158,7 @@ class Entrar_coletor(Screen):
                         print("Coletor encontrado com o e-mail", email)
                         break
                 if coletor_encontrado:
-                    self.mensagem = f"[b]Bem-vindo, {coletor_encontrado['nome']}[/b]!"
+                    self.mensagem = f"[b]Bem-vindo, {coletor_encontrado['usuario']}[/b]!"
                     boas_vindas_label = Label(text=self.mensagem, markup=True, color=(0, 0, 0, 1), font_size=24, pos_hint={'center_x': 0.5, 'center_y': 0.24})
                     self.ids.layout.add_widget(boas_vindas_label)
                 else:
@@ -200,7 +201,7 @@ class Entrar_usuario(Screen):
                         print("Usuário encontrado com o e-mail", email)
                         break
                 if usuario_encontrado:
-                    self.mensagem = f"[b]Bem-vindo, {usuario_encontrado['nome']}[/b]!"
+                    self.mensagem = f"[b]Bem-vindo, {usuario_encontrado['usuario']}[/b]!"
                     boas_vindas_label = Label(text=self.mensagem, markup=True, color=(0, 0, 0, 1), font_size=24, pos_hint={'center_x': 0.5, 'center_y': 0.24})
                     self.ids.layout.add_widget(boas_vindas_label)
                 else:
@@ -238,20 +239,81 @@ class ColetaMarker(MapMarker):
     pass
 
 class Empresa(Screen):
-    def salvar_atualizacao(self):
-        empresa = self.ids.nome_empresa.text
-        cnpj = self.ids.CNPJ.text
-        logadouro = self.ids.logadouro.text
-        numero = self.ids.numero.text
-        estado = self.ids.estado.text
-        bairro = self.ids.bairro.text
-        print(f"Perfil atualizado: Empresa={empresa}, CNPJ={cnpj}, Logadouro={logadouro}, numero={numero}, Estado= {estado}, Bairro={bairro}")
-
+    def empresa_atualizacoao(self):
+        empresa_novo = self.ids.empresa_novo.text
+        cnpj_novo = self.ids.CNPJ_novo.text
+        logadouro_novo = self.ids.logadouro_novo.text
+        numero_novo = self.ids.numero_novo.text
+        estado_novo = self.ids.estado_novo.text
+        bairro_novo = self.ids.bairro_novo.text
+        
+        if logadouro_novo and numero_novo and estado_novo and bairro_novo and empresa_novo and cnpj_novo:
+            email = self.manager.get_screen('login_coletor').ids.email.text
+            if self.app.atualizar_coletor(email, logadouro_novo, numero_novo, estado_novo, bairro_novo,empresa_novo,cnpj_novo):
+                self.manager.current = 'menu_coletor'
+            else:
+                toast('Erro: Não foi possível atualizar o perfil.')
+                
+        else:
+            toast('Erro: Preencha todos os campos.')
+    
 class Menu_coletor(Screen):
-    pass
+    mensagem = StringProperty("")
+
+    def on_enter(self, *args):
+        super(Menu_coletor, self).on_enter(*args)
+        email = self.manager.get_screen('login_coletor').ids.email_input.text
+        try:
+            coletores = db.child("schedule").get().val()  
+            if coletores:
+                coletor_encontrado = None
+                for key, value in coletores.items():
+                    if value.get("email") == email:
+                        coletor_encontrado = value
+                        print("Coletor encontrado com o e-mail", email)
+                        break
+                if coletor_encontrado:
+                    self.mensagem = f"[b]{coletor_encontrado['nome']}[b]"
+                    label = Label(text=self.mensagem, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint={'center_x': 0.5, 'center_y': 0.7})
+                    self.ids.layout.add_widget(label)
+                else:
+                    print("Nenhum coletor encontrado com o e-mail", email)
+            else:
+                print("Nenhum coletor encontrado")
+        except Exception as e:
+            print("Ocorreu um erro durante a consulta:", e)
+    
+    def coletor(self):
+        super(Menu_coletor, self).on_enter()
 
 class Menu_usuario(Screen):
-    pass
+    mensagem = StringProperty("")
+
+    def on_enter(self, *args):
+        super(Menu_usuario, self).on_enter(*args)
+        email = self.manager.get_screen('login_usuario').ids.email.text
+        try:
+            usuarios = db.child("schedule").get().val()  
+            if usuarios:
+                usuario_encontrado = None
+                for key, value in usuarios.items():
+                    if value.get("email") == email:
+                        usuario_encontrado = value
+                        print("Coletor encontrado com o e-mail", email)
+                        break
+                if usuario_encontrado:
+                    self.mensagem = f"[b]{usuario_encontrado['nome']}[b]"
+                    label = Label(text=self.mensagem, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint={'center_x': 0.5, 'center_y': 0.7})
+                    self.ids.layout.add_widget(label)
+                else:
+                    print("Nenhum coletor encontrado com o e-mail", email)
+            else:
+                print("Nenhum coletor encontrado")
+        except Exception as e:
+            print("Ocorreu um erro durante a consulta:", e)
+    
+    def usuario(self):
+        super(Menu_usuario, self).on_enter()
 
 class Central_ajuda(Screen):
     pass
@@ -263,26 +325,46 @@ class promocoes(Screen):
     pass
 
 class Endereco (Screen):
-    pass
+    def salvar_endereco(self):
+        logadouro_novo = self.ids.logadouro_novo.text
+        numero_novo = self.ids.numero_novo.text
+        estado_novo = self.ids.estado_novo.text
+        bairro_novo = self.ids.bairro_novo.text
+        
+        if logadouro_novo and numero_novo and estado_novo and bairro_novo:
+            email = self.manager.get_screen('login_usuario').ids.email.text
+            if self.app.atualizar_usuario(email, logadouro_novo, numero_novo, estado_novo, bairro_novo):
+                self.manager.current = 'editar_conta'
+            else:
+                toast('Erro: Não foi possível atualizar o perfil.')
+                
+        else:
+            toast('Erro: Preencha todos os campos.')
+
 
 class Central_ajuda_coletor(Screen):
     pass
 
 class Solicitacoes(Screen):
     def escolha_usuario(self):
-        self.manager.current = 'escolher_solicitacao'
+        self.manager.current = 'perfil_do_usuario'
 
-class Escolher_solicitacao(Screen):
-    pass
 
 class Editar_conta_coletor(Screen):
     def salvar_perfil(self):
-        bio = self.ids.bio.text
-        nome = self.ids.nome.text
-        genero = self.ids.genero.text
-        link = self.ids.link.text
-
-        print(f"Perfil atualizado: Nome={nome}, Bio={bio}, Gênero={genero}, Link={link}")
+        novo_nome = self.ids.nome.text
+        novo_genero = self.ids.genero.text
+        novo_tel = self.ids.tel.text
+        
+        if novo_nome and novo_genero and novo_tel:
+            email = self.manager.get_screen('login_coletor').ids.email.text
+            if self.app.atualizar_coletor(email, novo_nome, novo_genero, novo_tel):
+                self.manager.current = 'perfil_do_coletor'
+            else:
+                toast('Erro: Não foi possível atualizar o perfil.')
+                
+        else:
+            toast('Erro: Preencha todos os campos.')
 
 class Editar_conta(Screen):
 
@@ -301,9 +383,9 @@ class Editar_conta(Screen):
                 self.manager.current = 'perfil_do_usuario'
             else:
                 toast('Erro: Não foi possível atualizar o perfil.')
+                
         else:
             toast('Erro: Preencha todos os campos.')
-
 
     def endereco(self):
         self.manager.current = 'endereco'
@@ -320,12 +402,90 @@ class Area_coletores(Screen):
         self.manager.current = 'selecionar_coletores'
 
 class Perfil_do_coletor(Screen):
+    nome = StringProperty("")
+    empresa = StringProperty("")
+    logadouro = StringProperty("")
+    numero = StringProperty("")
+
+    def on_enter(self, *args):
+        super(Perfil_do_coletor, self).on_enter(*args)
+
+        email_coletor = "apollo.santana@gmail.com"
+        try:
+            coletor_info = db.child("schedule").get().val()  
+            if coletor_info:
+                coletor_encontrado = None
+                for key, value in coletor_info.items():
+                    if value.get("email") == email_coletor:
+                        coletor_encontrado = value
+                        print("Coletor encontrado com o e-mail", email_coletor)
+                        break
+                if coletor_encontrado:
+                    self.nome = f"[b]{coletor_encontrado['nome']}[b]"
+                    label = Label(text=self.nome, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.5, 'center_y': 0.57})
+                    self.ids.layout.add_widget(label)
+                    self.empresa = f"[b]{coletor_encontrado['empresa']}[b]"
+                    label_empresa = Label(text=self.empresa, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.5, 'center_y': 0.5})
+                    self.ids.layout.add_widget(label_empresa)
+                    self.logadouro = f"[b]{coletor_encontrado['logadouro']},[b]"
+                    label_logadouro = Label(text=self.logadouro, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.4, 'center_y': 0.3})
+                    self.ids.layout.add_widget(label_logadouro)
+                    self.numero = f"[b]{coletor_encontrado['numero']}[b]"
+                    label_numero = Label(text=self.numero, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.8, 'center_y': 0.3})
+                    self.ids.layout.add_widget(label_numero)
+                    self.bairro = f"[b]{coletor_encontrado['bairro']}[b]"
+                    label_bairro = Label(text=self.bairro, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.5, 'center_y': 0.25})
+                    self.ids.layout.add_widget(label_bairro)
+                else:
+                    print("Nenhum coletor encontrado com o e-mail", email_coletor)
+            else:
+                print("Nenhum coletor encontrado")
+        except Exception as e:
+            print("Ocorreu um erro durante a consulta:", e)
+
     def coletores_selecao(self):
         self.manager.current = 'chat_usuario'
 
 
 class Perfil_do_usuario(Screen):
-    pass
+    nome = StringProperty("")
+    logadouro = StringProperty("")
+    numero = StringProperty("")
+    bairro = StringProperty("")
+
+    def on_enter(self, *args):
+        super(Perfil_do_usuario, self).on_enter(*args)
+
+        email_usuario = "giovana890@gmail.com"
+        try:
+            usuario_info = db.child("schedule").get().val()  
+            if usuario_info:
+                usuario_encontrado = None
+                for key, value in usuario_info.items():
+                    if value.get("email") == email_usuario:
+                        usuario_encontrado = value
+                        print("Usuário encontrado com o e-mail", email_usuario)
+                        break
+                if usuario_encontrado:
+                    self.nome = f"[b]{usuario_encontrado['nome']}[b]"
+                    label = Label(text=self.nome, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.5, 'center_y': 0.57})
+                    self.ids.layout.add_widget(label)
+                    self.logadouro = f"[b]{usuario_encontrado['logadouro']},[b]"
+                    label_logadouro = Label(text=self.logadouro, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.4, 'center_y': 0.3})
+                    self.ids.layout.add_widget(label_logadouro)
+                    self.numero = f"[b]{usuario_encontrado['numero']}[b]"
+                    label_numero = Label(text=self.numero, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.75, 'center_y': 0.3})
+                    self.ids.layout.add_widget(label_numero)
+                    self.bairro = f"[b]{usuario_encontrado['bairro']}[b]"
+                    label_bairro = Label(text=self.bairro, markup=True, color=(0, 0, 0, 1), font_size=28, pos_hint= {'center_x': 0.5, 'center_y': 0.25})
+                    self.ids.layout.add_widget(label_bairro)
+                else:
+                    print("Nenhum usuário encontrado com o e-mail", email_usuario)
+            else:
+                print("Nenhum usuário encontrado")
+        except Exception as e:
+            print("Ocorreu um erro durante a consulta:", e)
+
 
 #passo 5: fazer o chat
 
@@ -356,6 +516,7 @@ class Gerenciador(ScreenManager):
     pass
 
 class ReciclatechApp(MDApp):
+    db = firebase.database()
     def build(self):
         gerenciador = Gerenciador()
         gerenciador.add_widget(Inicial(name='inicial'))
@@ -383,13 +544,12 @@ class ReciclatechApp(MDApp):
         gerenciador.add_widget(Central_ajuda(name='central_ajuda'))
         gerenciador.add_widget(Endereco(name='endereco'))
         gerenciador.add_widget(Editar_conta_coletor(name='editar_conta_coletor'))
-        gerenciador.add_widget(Escolher_solicitacao(name='escolher_solicitacao'))
         gerenciador.add_widget(Cadastro_usuario(name='registrar_usuario'))
         gerenciador.add_widget(Cadastro_coletor(name='registrar_coletor'))
   
         return gerenciador
     
-    def cadastrar_usuario(self, nome, genero, cpf_cnpj, tel, logadouro, numero, estado, bairro, email, senha):
+    def cadastrar_usuario(self, nome, genero, cpf_cnpj, tel, logadouro, numero, estado, bairro, email, senha, usuario):
         if not self.email_verificacao_usu(email):
             db.child("/schedule/").push({
                 'nome': nome,
@@ -401,18 +561,19 @@ class ReciclatechApp(MDApp):
                 'estado': estado,
                 'bairro': bairro,
                 'email': email,
-                'senha': senha
+                'senha': senha,
+                'usuario': usuario
                 })
             return True
         else:
             return False
         
-    def cadastrar_coletor(self, nome, genero, cpf, cnpj, empresa, tel, logadouro, numero,estado, bairro, email, senha):
+    def cadastrar_coletor(self, nome, genero, usuario, cnpj, empresa, tel, logadouro, numero,estado, bairro, email, senha):
         if not self.email_verificacao_col(email):
             db.child("/schedule/").push({
                 'nome': nome,
                 'genero': genero,
-                'cpf': cpf,
+                'usuario': usuario,
                 'tel': tel,
                 'empresa': empresa,
                 'cnpj': cnpj,
@@ -481,6 +642,42 @@ class ReciclatechApp(MDApp):
         else:
             toast('Erro: Preencha todos os campos.')
 
+    def atualizar_usuario(self, email, novo_nome, novo_genero, novo_tel, logadouro_novo, numero_novo, estado_novo, bairro_novo):
+        # Primeiro, obtenha a chave do usuário com base no e-mail
+        usuarios = self.db.child("schedule").get().val()
+        if usuarios:
+            for chave, usuario in usuarios.items():
+                if usuario['email'] == email:
+                    # Atualize os dados do usuário com os novos valores
+                    self.db.child("schedule").child(chave).update({
+                        'nome': novo_nome,
+                        'genero': novo_genero,
+                        'tel': novo_tel,
+                        'logadouro': logadouro_novo,
+                        'numero': numero_novo,
+                        'estado': estado_novo,
+                        'bairro': bairro_novo
+                    })
+                    return True
+        return False
+    
+    def atualizar_coletor(self, email, novo_nome, novo_genero, novo_tel, CNPJ_novo, logadouro_novo, numero_novo, estado_novo, bairro_novo):
+        coletores = self.db.child("schedule").get().val()
+        if coletores:
+            for chave, coletor in coletores.items():
+                if coletor['email'] == email:
+                    self.db.child("schedule").child(chave).update({
+                        'nome': novo_nome,
+                        'genero': novo_genero,
+                        'tel': novo_tel,
+                        'cnpj': CNPJ_novo,
+                        'logadouro': logadouro_novo,
+                        'numero': numero_novo,
+                        'estado': estado_novo,
+                        'bairro': bairro_novo,
+                    })
+                    return True
+        return False
       
 if __name__ == '__main__':
     ReciclatechApp().run()
